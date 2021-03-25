89e2c7cd96bc80b3d1b12b072c71bb93bb544604
xzs603 <xzs603@163.com>
2021-03-25 20:26:13 +0800

    remove thymeleaf

--- src/main/java/com/test/activiti/activiti/controller/PageController.java
+++ src/main/java/com/test/activiti/activiti/controller/PageController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class PageController {
-    @GetMapping("editor")
+    @GetMapping("/editor")
     public String test(){
-        return "/modeler";
+        return "modeler";
     }
 }
